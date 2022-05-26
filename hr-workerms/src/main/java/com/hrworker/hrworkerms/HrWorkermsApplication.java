af126ec6419b327c2ec0b0c8483ef38422d66a57
Murilo <murilo.ferreira@tdadev.com.br>
2022-05-26 01:52:19 -0300

    project hr-worker created

---
+++ hr-workerms/src/main/java/com/hrworker/hrworkerms/HrWorkermsApplication.java
@@ -0,0 +1,13 @@
+package com.hrworker.hrworkerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkermsApplication.class, args);
+	}
+
+}
---
+++ hr-workerms/src/test/java/com/hrworker/hrworkerms/HrWorkermsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrworker.hrworkerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
