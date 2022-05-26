64e31e7d5b44947d9d894b037bd50ac4f55dd001
Murilo <murilo.ferreira@tdadev.com.br>
2022-05-26 13:04:12 -0300

    project hr-payroll created

--- hr-workerms/src/main/resources/_Mydata.sql
+++ hr-workerms/src/main/resources/_Mydata.sql
@@ -1,8 +1,3 @@
-create table tb_worker(
-   id int primary key auto_increment,
-   name varchar(100) not null,
-   dailyIncome varchar(100) not null
-);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
 INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
